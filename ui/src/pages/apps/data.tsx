5a95c3a217ea93e46b3522ccc337c09c67e3f3b3
DanialSobri <adanial091@gmail.com>
2024-06-09 14:10:25 +0800

    Initial commit

---
+++ ui/src/pages/apps/data.tsx
@@ -0,0 +1,154 @@
+import {
+  IconBrandDiscord,
+  IconBrandDocker,
+  IconBrandFigma,
+  IconBrandGithub,
+  IconBrandGitlab,
+  IconBrandGmail,
+  IconBrandMedium,
+  IconBrandNotion,
+  IconBrandSkype,
+  IconBrandSlack,
+  IconBrandStripe,
+  IconBrandTelegram,
+  IconBrandTrello,
+  IconBrandWhatsapp,
+  IconBrandZoom,
+} from '@tabler/icons-react'
+
+export const family = [
+  {
+    name: "Gallon Fuel Container",
+    desc: "Plastic fuel can",
+    freemium: "free",
+    cover:
+      "https://plugin-storage.nyc3.digitaloceanspaces.com/families/images/clw5b17ae7pvalh0roa5241ho.jpg",
+  },
+  {
+    name: "Halo Mirror",
+    desc: "Full-length mirror with wooden frame, by Brecht Wright Gander",
+    cover:
+      "https://plugin-storage.nyc3.digitaloceanspaces.com/families/images/dabf7ca2-a2e7-4f27-b969-5f561303c0c7.jpg",
+  },
+  {
+    name: "Topazio Finestra Wardrobe",
+    desc: "Wardrobe with 3 doors and 4 drawers, wood",
+    freemium: "free",
+    cover:
+      "https://plugin-storage.nyc3.digitaloceanspaces.com/families/images/clwezctdr7pyclh0rxgh3o5iq.jpg",
+  },
+  {
+    name: "Shelves",
+    desc: "Shelf with 3 supports",
+    freemium: "free",
+    cover:
+      "https://plugin-storage.nyc3.digitaloceanspaces.com/families/images/d5dc05f5-1fce-4404-89d9-91b04c4c6210.jpg",
+  },
+  {
+    name: "Curved Kitchen Island Countertop",
+    desc: "Curved sculpted kitchen island countertop with wood base",
+    freemium: "free",
+    cover:
+      "https://plugin-storage.nyc3.digitaloceanspaces.com/families/images/6b6766ee-8c6e-43ab-9a93-316ac4d7b105.jpg",
+  },
+  {
+    name: "Toilet Bowl",
+    desc: "Toilet bowl with built-in cistern, made of ceramic",
+    freemium: "premium",
+    cover:
+      "https://plugin-storage.nyc3.digitaloceanspaces.com/families/images/clvle41a07p3elh0r78adcvp1.jpg",
+  }
+]
+
+export const apps = [
+  {
+    name: 'Telegram',
+    logo: <IconBrandTelegram />,
+    connected: false,
+    desc: 'Connect with Telegram for real-time communication.',
+  },
+  {
+    name: 'Notion',
+    logo: <IconBrandNotion />,
+    connected: true,
+    desc: 'Effortlessly sync Notion pages for seamless collaboration.',
+  },
+  {
+    name: 'Figma',
+    logo: <IconBrandFigma />,
+    connected: true,
+    desc: 'View and collaborate on Figma designs in one place.',
+  },
+  {
+    name: 'Trello',
+    logo: <IconBrandTrello />,
+    connected: false,
+    desc: 'Sync Trello cards for streamlined project management.',
+  },
+  {
+    name: 'Slack',
+    logo: <IconBrandSlack />,
+    connected: false,
+    desc: 'Integrate Slack for efficient team communication',
+  },
+  {
+    name: 'Zoom',
+    logo: <IconBrandZoom />,
+    connected: true,
+    desc: 'Host Zoom meetings directly from the dashboard.',
+  },
+  {
+    name: 'Stripe',
+    logo: <IconBrandStripe />,
+    connected: false,
+    desc: 'Easily manage Stripe transactions and payments.',
+  },
+  {
+    name: 'Gmail',
+    logo: <IconBrandGmail />,
+    connected: true,
+    desc: 'Access and manage Gmail messages effortlessly.',
+  },
+  {
+    name: 'Medium',
+    logo: <IconBrandMedium />,
+    connected: false,
+    desc: 'Explore and share Medium stories on your dashboard.',
+  },
+  {
+    name: 'Skype',
+    logo: <IconBrandSkype />,
+    connected: false,
+    desc: 'Connect with Skype contacts seamlessly.',
+  },
+  {
+    name: 'Docker',
+    logo: <IconBrandDocker />,
+    connected: false,
+    desc: 'Effortlessly manage Docker containers on your dashboard.',
+  },
+  {
+    name: 'GitHub',
+    logo: <IconBrandGithub />,
+    connected: false,
+    desc: 'Streamline code management with GitHub integration.',
+  },
+  {
+    name: 'GitLab',
+    logo: <IconBrandGitlab />,
+    connected: false,
+    desc: 'Efficiently manage code projects with GitLab integration.',
+  },
+  {
+    name: 'Discord',
+    logo: <IconBrandDiscord />,
+    connected: false,
+    desc: 'Connect with Discord for seamless team communication.',
+  },
+  {
+    name: 'WhatsApp',
+    logo: <IconBrandWhatsapp />,
+    connected: false,
+    desc: 'Easily integrate WhatsApp for direct messaging.',
+  },
+]
